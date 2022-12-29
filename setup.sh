5c9701b04905d0ae8b99cca143164f71095abf8d
pupattan <pkbsdmp@gmail.com>
2022-12-29 13:15:34 +0530

    Updating sh file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
-export DATA_DIR=$1
+#!/bin/bash
+if [ $# -ge 1 ]
+then
+    DATA_DIR=$1
+else
+    DATA_DIR="$HOME/websites"
+fi
 mkdir ${DATA_DIR}
 mkdir ${DATA_DIR}/conf
 mkdir ${DATA_DIR}/vhost
 mkdir ${DATA_DIR}/html
 mkdir ${DATA_DIR}/dhparam
 mkdir ${DATA_DIR}/certs
-
+wget https://raw.githubusercontent.com/pupattan/nginx-proxy-docker-deploy-setup/master/dc-nginx-setup.yaml
